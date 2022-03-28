5a6a2e8edc4b383d26736eac6e128f1eeae8d0f0
Doenerlord <50668362+Doenerlord@users.noreply.github.com>
2022-03-28 12:23:54 +0200

    added events

--- EnginEngine/src/Engin/Application.cpp
+++ EnginEngine/src/Engin/Application.cpp
@@ -1,17 +1,26 @@
 #include "enginpch.h"
 #include "Application.h"
-#include "Engin/Events/ApplicationEvent.h"
 #include "Engin/Log.h"
 #include <GLFW/glfw3.h>
 
 namespace Engin {
 
+#define  BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() {
 
+	}
+
+	void Application::OnEvent(Event& e) {
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		ENGIN_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run() {
@@ -21,5 +30,10 @@ namespace Engin {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
 	}
 }
--- EnginEngine/src/Engin/Events/Event.h
+++ EnginEngine/src/Engin/Events/Event.h
@@ -32,6 +32,7 @@ namespace Engin {
 #define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override {return category; }
 
 	class ENGIN_API Event {
+		friend class EventDispatcher;
 	public:
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
